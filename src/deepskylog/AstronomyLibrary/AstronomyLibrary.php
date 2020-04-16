e424b84ff3891b4cd2c17b4e4c02d1518d69c164
Wim De Meester <deepskywim@gmail.com>
2020-04-16 20:20:40 +0200

    Add constructor for AstronomyLibrary which takes a date as argument.

--- src/deepskylog/AstronomyLibrary/AstronomyLibrary.php
+++ src/deepskylog/AstronomyLibrary/AstronomyLibrary.php
@@ -1,8 +1,42 @@
 <?php
+/**
+ * The main AstronomyLibrary class.
+ *
+ * PHP Version 7
+ *
+ * @category AstronomyLibrary
+ * @package  AstronomyLibrary
+ * @author   Deepsky Developers <developers@deepskylog.be>
+ * @license  GPL3 <https://opensource.org/licenses/GPL-3.0>
+ * @link     http://www.deepskylog.org
+ */
 
 namespace deepskylog\AstronomyLibrary;
 
+use Carbon\Carbon;
+
+/**
+ * The main AstronomyLibrary class.
+ *
+ * PHP Version 7
+ *
+ * @category AstronomyLibrary
+ * @package  AstronomyLibrary
+ * @author   Deepsky Developers <developers@deepskylog.be>
+ * @license  GPL3 <https://opensource.org/licenses/GPL-3.0>
+ * @link     http://www.deepskylog.org
+ */
 class AstronomyLibrary
 {
-    // Build wonderful things
+    private $_date;
+
+    /**
+     * The constructor.
+     *
+     * @param Carbon $carbonDate The date
+     */
+    public function __construct(Carbon $carbonDate)
+    {
+        $this->_date = $carbonDate;
+    }
 }
